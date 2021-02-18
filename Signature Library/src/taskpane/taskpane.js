2578c0f07a6543f15da1a481ceccf3728e789d09
Emchtastic <alex80214@gmail.com>
2021-02-18 12:33:07 -0700

    Deleted free .js .html and .css files so the only copies are within the "Signature Library" folder

--- Signature Library/src/taskpane/taskpane.js
+++ Signature Library/src/taskpane/taskpane.js
@@ -8,16 +8,41 @@ import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
 
-/* global document, Office */
 
 Office.onReady(info => {
-  if (info.host === Office.HostType.Outlook) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
+	if (info.host === Office.HostType.Outlook) {
+		document.getElementById("addToDrop").onclick = addToDroplist;
+		document.getElementById("dropButton").onclick = myFunction;
   
-  }
+	}
 });
 
+function addToDroplist() {
+
+	var node = document.createElement("Li");
+	var text = document.getElementById("title_input").value; 
+	var textnode=document.createTextNode(text);
+	node.appendChild(textnode);
+	document.getElementById("dropList").appendChild(node);
+}
+
+function myFunction() {
+	document.getElementById("dropList").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches(".dropbtn")) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains("show")) {
+				openDropdown.classList.remove("show");
+			}
+		}
+	}
+};
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
