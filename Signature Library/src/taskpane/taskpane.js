6e7366c8550a446f984190760ccabd72ec4adb28
lfry12 <lfry12@gmail.com>
2021-03-04 21:22:14 -0700

    Update taskpane.js
    Added in the code from dev (my branch was not uploaded to the newest version of the taskpane.js).
    
    Added createCookie to the elements hardcoded signatureList
    
    Added createCookie to the addToLib method.
    
    Added deleteCookie to the removeInList method.

--- Signature Library/src/taskpane/taskpane.js
+++ Signature Library/src/taskpane/taskpane.js
@@ -4,51 +4,109 @@
  */
 
 // images references in the manifest
-import "../../assets/icon-16.png";
-import "../../assets/icon-32.png";
-import "../../assets/icon-80.png";
+//import "../../assets/icon-16.png";
+//import "../../assets/icon-32.png";
+//import "../../assets/icon-80.png";
 
-var signatureList = []
+var signatureList = [{
+	title : "Yoda",
+	message : "“The greatest teacher, failure is.”\n ---Yoda"
+},
+{
+	title : "Vader",
+	message : "“All that I sense,\n is fear and dead men.”\n ---Darth Vader"
+},
+{
+	title : "Han Solo",
+	message : "“It’s not wise to upset a Wookiee.”\n ---Han Solo"
+}]
+
+createCookie("Yoda", "“The greatest teacher, failure is.”\n ---Yoda");
+createCookie("Vader", "“All that I sense,\n is fear and dead men.”\n ---Darth Vader");
+createCookie("Han Solo", "“It’s not wise to upset a Wookiee.”\n ---Han Solo")
 
 
 Office.onReady(info => {
 	if (info.host === Office.HostType.Outlook) {
-		document.getElementById("dropButton").onclick = myFunction;
-		document.getElementById("removeLast").onclick = removeLastInList;
+		document.getElementById("removeThis").onclick = removeInList;
 		document.getElementById("addToLib").onclick  = addToLib;
 		document.getElementById("showLib").onclick  = showLibrary;
+		document.getElementById("applySignatureButton").onclick  = applySignature;
+		document.getElementById("imFeelingLucky").onclick  = applyRandomSignature;
 
   
 	}
 });
 
 function addToLib() {
+	// A function that creates a new object with title and message then adds it to the signature array
 	var newSignature = {title : document.getElementById("title_input").value, 
         message : document.getElementById("message_input").value
         }
 
-        signatureList.push(newSignature)
-	
-	var node = document.createElement("Li");
-	var text = document.getElementById("title_input").value; 
-	var textnode=document.createTextNode(text);
-	node.appendChild(textnode);
-	document.getElementById("dropList").appendChild(node);
+        signatureList.push(newSignature);
+		
+		var updatedDropdown = document.getElementById("signatures");
+		var option = document.createElement("option");
+		option.value = newSignature.title;
+		updatedDropdown.appendChild(option);
+		createCookie(newSignature.title, newSignature.message);
+
 }
 
 function showLibrary() {
 	// Need method for window to pop up with signatureList elements
-	document.getElementById("Library").innerHTML = JSON.stringify(signatureList)
+	var libraryList = ""
+	
+	for (let signature of signatureList){
+		libraryList = libraryList + signature.title + "&emsp;" + signature.message + "<BR/>" 
+	}
+
+	document.getElementById("Library").innerHTML = libraryList;
 }
 
-function myFunction() {
-	document.getElementById("dropList").classList.toggle("show");
+function removeInList() {
+	// Removes the chosen element in the dropList
+	var title = document.querySelector('#signature').value;
+	var y = document.getElementById("signatures");
+	deleteCookie(title);
+
+	var i;
+	
+	for (i = 0; i < signatureList.length; i++){
+		if (signatureList[i].title == title) {
+			signatureList.splice(i, 1)
+			y.children[i].remove()
+		}
+
+	}
+	showLibrary();
 }
 
-function removeLastInList() {
-	// Removes the last element in the dropList
-	var x = document.getElementById("dropList");
-	x.removeChild(x.lastElementChild);
+function applySignature(){
+	var title = document.querySelector('#signature').value;
+	var emailMessage = null
+
+	var i;
+	for (i = 0; i < signatureList.length; i++){
+		if (signatureList[i].title == title) {
+			emailMessage = signatureList[i].message
+		}
+	}
+
+	Office.context.mailbox.item.body.setSelectedDataAsync(emailMessage)
+
+}
+
+function getRandom() {
+	return Math.floor(Math.random()* (signatureList.length)+1)
+  }
+
+function applyRandomSignature(){
+	var chosenSignature = signatureList[getRandom()-1];
+	var message = chosenSignature.message
+
+	Office.context.mailbox.item.body.setSelectedDataAsync(message)
 }
 
 function createCookie(title, signature) {
@@ -79,23 +137,15 @@ function deleteCookie(name) {
     var expires = "expires=" + date.toGMTString();
     document.cookie = title + "=" + signature + ";" + expires + ";path=/";
 }
-
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-	if (!event.target.matches(".dropbtn")) {
-		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains("show")) {
-				openDropdown.classList.remove("show");
-			}
-		}
-	}
-};
+// Cookie methods are courtesy https://www.codexworld.com/store-data-in-cookies-with-javascript/ with minor modificaitons.
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-
-
+module.exports = {
+	getRandom : getRandom ,
+	applyRandomSignature : applyRandomSignature,
+	applySignature : applySignature,
+	showLibrary : showLibrary ,
+	removeInList : removeInList,
+	addToLib : addToLib,
+}
