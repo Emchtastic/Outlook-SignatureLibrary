e5f27a7caec134fdd10188e8cac216aac3cd4ddf
Sarmad Tello <stello1@msudenver.edu>
2021-03-07 00:36:59 +0000

    test.spec.js edited online with Bitbucket

--- Signature Library/src/taskpane/_test_/test.spec.js
+++ Signature Library/src/taskpane/_test_/test.spec.js
@@ -1,3 +1,22 @@
 
 var add = require('.');
 add.
+
+/*describe('apply Signature test', ()=>{
+it('title should not be null',()=>{
+const title = DM.window.document.getElementById('signature');
+expect(title).toBeDefined();
+});
+it('email Message Should equal singature with title',()=>{
+const _title = 'hello';
+const singatureList= [
+{title: 'hello', message: 'Message'}
+];
+let _message = null;
+sinatureList.forEach(({title, message})=>{
+if (_title === title)_message = message;
+});
+expect(_message).toEqual('Message');
+});
+it ('message should be null',()=>{});
+}); 
