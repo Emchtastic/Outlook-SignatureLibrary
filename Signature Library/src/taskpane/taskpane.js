278c771c43c8a35e01f761f6116781da2683e6c3
MARIEDIAOUNE <marie.diaoune@gmail.com>
2021-03-08 15:44:18 -0700

    Fixed the placement of the "Add new sign to library""
    The Button is spaced out and fixed the sized so that the text is on one line

--- Signature Library/src/taskpane/taskpane.js
+++ Signature Library/src/taskpane/taskpane.js
@@ -100,18 +100,18 @@ function applyRandomSignature(){
 toggle between hiding and showing the dropdown content */
 
 
-function openSig(evt, sigantureName) {
+function openSig(evt, signatureName) {
 
 	var i, tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
-	  tabcontent[i].style.display = "none";
+	  tabcontent[i].style.display = "";
 	}
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 	  tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
-	document.getElementById(cityName).style.display = "block";
+	document.getElementById(signatureName).style.display = "block";
 	evt.currentTarget.className += " active";
   
   // Get the element with id="defaultOpen" and click on it
