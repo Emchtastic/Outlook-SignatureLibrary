638450e48e98786a8d5a5c8bae0213eea8464f5a
MARIEDIAOUNE <marie.diaoune@gmail.com>
2021-03-08 13:55:56 -0700

    Adding the a divided window
    This bring up a window that's divided into two. One side is a list of placeholder for the signatures and other side is the the contents of each placeholder.

--- Signature Library/src/taskpane/taskpane.js
+++ Signature Library/src/taskpane/taskpane.js
@@ -98,3 +98,22 @@ function applyRandomSignature(){
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+
+
+function openSig(evt, sigantureName) {
+
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+}
