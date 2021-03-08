aaac6ade7c8c7911866f86bfc0320b93259606d7
lfry12 <lfry12@gmail.com>
2021-03-08 12:18:04 -0700

    Update taskpane.js
    added a getCookie method that should pull all cookies from local.
    
    Commented out the readCookie method but retained in case we might need it later.

--- Signature Library/src/taskpane/taskpane.js
+++ Signature Library/src/taskpane/taskpane.js
@@ -116,20 +116,30 @@ function createCookie(title, signature) {
     document.cookie = title + "=" + signature + ";" + expires + ";path=/";
 }
 
-function readCookie(name) {
-    var key = name + "=";
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1, cookie.length);
-        }
-        if (cookie.indexOf(key) == 0) {
-            return cookie.substring(key.length, cookie.length);
-        }
-    }
-    return null;
-}
+//function readCookie(name) {
+    //var key = name + "=";
+    //var cookies = document.cookie.split(';');
+    //for (var i = 0; i < cookies.length; i++) {
+       //var cookie = cookies[i];
+        //while (cookie.charAt(0) == ' ') {
+            //cookie = cookie.substring(1, cookie.length);
+        //}
+        //if (cookie.indexOf(key) == 0) {
+            //return cookie.substring(key.length, cookie.length);
+        //}
+    //}
+    //return null;
+//}
+
+var getCookies = function(){
+	var pairs = document.cookie.split(";");
+	var cookies = {};
+	for (var i=0; i<pairs.length; i++){
+	  var pair = pairs[i].split("=");
+	  cookies[(pair[0]+'').trim()] = unescape(pair.slice(1).join('='));
+	}
+	return cookies;
+  }
 
 function deleteCookie(name) {
     var date = new Date();
@@ -137,7 +147,7 @@ function deleteCookie(name) {
     var expires = "expires=" + date.toGMTString();
     document.cookie = title + "=" + signature + ";" + expires + ";path=/";
 }
-// Cookie methods are courtesy https://www.codexworld.com/store-data-in-cookies-with-javascript/ with minor modificaitons.
+// Cookie methods were helped with https://www.codexworld.com/store-data-in-cookies-with-javascript/ & https://stackoverflow.com/questions/252665/i-need-to-get-all-the-cookies-from-the-browser 
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
