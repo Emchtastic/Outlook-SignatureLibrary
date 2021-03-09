07c61850e6b67ea00df5ff2c6f2d82d14fdfd5b0
Alex Emch <aemch@msudenver.edu>
2021-03-09 22:42:12 +0000

    Fixed import of showLib function - no longer in use for testing

--- Signature Library/src/taskpane/taskpane.js
+++ Signature Library/src/taskpane/taskpane.js
@@ -123,24 +123,13 @@ function applyRandomSignature(){
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 module.exports = {
-    getRandom : getRandom ,
-    applyRandomSignature : applyRandomSignature,
-    applySignature : applySignature,
-    showLibrary : showLibrary ,
-    removeInList : removeInList,
-    addToLib : addToLib,
+	getRandom : getRandom ,
+	applyRandomSignature : applyRandomSignature,
+	applySignature : applySignature,
+	removeInList : removeInList,
+	addToLib : addToLib,
+	signatureList : signatureList,
 }
 
 // reference code: https://www.kirupa.com/html5/storing_and_retrieving_an_array_from_local_storage.htm, https://stackoverflow.com/questions/17745292/how-to-retrieve-all-localstorage-items-without-knowing-the-keys-in-advance
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-module.exports = {
-	getRandom : getRandom ,
-	applyRandomSignature : applyRandomSignature,
-	applySignature : applySignature,
-	showLibrary : showLibrary ,
-	removeInList : removeInList,
-	addToLib : addToLib,
-	signatureList  :  signatureList ,
-}
