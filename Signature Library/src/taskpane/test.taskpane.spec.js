007192a9913cb1e75b6bc0982ae93acddb78e319
Sarmad Tello <stello1@msudenver.edu>
2021-03-10 13:53:21 +0000

    test.taskpane.spec.js edited online with Bitbucket

--- Signature Library/src/taskpane/test.taskpane.spec.js
+++ Signature Library/src/taskpane/test.taskpane.spec.js
@@ -1,1 +1,19 @@
-
+var applySignature = import("applySignature");
+describe('apply Signature test', () => {
+    it('title should not be null',()=>{
+    const title = window.document.getElementById('signature');
+    expect(title).toBeDefined();
+    });
+    it('email Message Should equal singature with title',()=>{
+        const _title = 'hello';
+        const singatureList= [
+        {title: 'hello', message: 'Message'}
+        ]
+        let _message = null;
+        singatureList.forEach(({title, message})=>{
+        if (_title === title)_message = message;
+        });
+        expect(_message).toEqual('Message');
+        });
+        it ('message should be null',()=>{});
+        });
