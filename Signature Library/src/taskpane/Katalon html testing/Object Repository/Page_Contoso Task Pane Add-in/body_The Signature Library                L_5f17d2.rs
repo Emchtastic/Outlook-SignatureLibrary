295b5830914a1d00b0b6a0c0df90fa5af7411793
Gabriella Esquibel <gabbyesquibel@hotmail.com>
2021-04-12 15:00:22 -0500

    Drop down list test

---
+++ Signature Library/src/taskpane/Katalon html testing/Object Repository/Page_Contoso Task Pane Add-in/body_The Signature Library                L_5f17d2.rs
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_The Signature Library                L_5f17d2</name>
+   <tag></tag>
+   <elementGuidId>480c1b58-20e8-4cf5-92cd-d3a42f285aa5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*/text()[normalize-space(.)='Here are your signatures:']/parent::*</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>body.ms-font-m.ms-welcome.ms-Fabric</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>ms-font-m ms-welcome ms-Fabric</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+        The Signature Library
+    
+    
+        Let's add some signatures!
+    
+    
+        
+        
+    
+
+	Add new signature
+	
+  	  
+    	    
+      		Store new signature
+            
+      		
+            
+            
+            
+    	    
+
+    	    
+                Add new signature to library
+      	  	    Close
+        
+    	    
+  	    
+    
+    
+    
+
+    Show current signature library
+	
+  	  
+    	    
+            
+                Hold 1
+                Hold 2
+                Hold 3
+            
+              
+            
+                
+                
+            
+    	    
+
+    	    
+      	  	    Close
+    	    
+  	    
+    
+    
+
+    
+
+    
+    Here are your signatures:
+    
+        
+        
+          
+          
+          
+        
+        
+        
+        
+        
+      
+      
+    
+    
+
+
+
+
+
+/html[1]/body[@class=&quot;ms-font-m ms-welcome ms-Fabric&quot;]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;ms-font-m ms-welcome ms-Fabric&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Here are your signatures:']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
