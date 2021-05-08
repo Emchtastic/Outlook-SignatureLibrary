445f5bc10178fd45bd3a2d8dfdbe34482118e838
lfry12 <lfry12@gmail.com>
2021-05-08 12:02:32 -0600

    Update test.taskpane.spec.js
    Added array to test to get test to run properly

--- Signature Library/src/taskpane/test.taskpane.spec.js
+++ Signature Library/src/taskpane/test.taskpane.spec.js
@@ -122,12 +122,15 @@ describe("clearAllMocksa and resetModules ", () => {
   describe("Test allStorage", () => {
     beforeAll(() => {
       document.body.innerHTML = `<div id="signatures"></div>`;
-      roamingSettings.get("test", "test");
+      roamingSettings.set("test", "test");
     });
-    beforeEach(() => roamSignatures.clear());
     it("adds items to the roaming storage", () => {
+      const test = [{
+        title : "test",
+        message : "test"
+      }]
       const initialOptionsCount = document.getElementById("signatures").childElementCount;
-      roamSignatures.set("test", "test");
+      roamSignatures.set("signatures", test);
       const taskpane = require("./taskpane");
       const signatureList = taskpane.signatureList;
       taskpane.allStorage();
